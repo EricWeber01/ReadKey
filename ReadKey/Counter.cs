67f082bfd2adf3b34eac3e21b83a6d475507b200
babya <babya@DESKTOP-6H8S8H4>
2022-04-25 11:10:57 +0300

    Add project files.

---
+++ ReadKey/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadKey
+{
+    class Counter
+    {
+        int cnt = 0;
+        int push = 0;
+        public void Action(ConsoleKeyInfo key)
+        {
+            if (key.Key == ConsoleKey.OemPlus)
+            {
+                Console.Clear();
+                cnt++;
+                push++;
+                Console.WriteLine($"{cnt}");
+            }
+            else if (key.Key == ConsoleKey.Backspace)
+            {
+                Console.Clear();
+                cnt--;
+                push++;
+                Console.WriteLine($"{cnt}");
+            }
+            else if (key.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine($"Счетчик - {cnt}\nКол-во нажатий - {push}\n");
+                Thread.Sleep(2000);
+                Console.Clear();
+            }
+            else if (key.Key == ConsoleKey.Enter)
+                Console.WriteLine("\n---ВЫХОД---\n");
+        }
+    }
+}
---
+++ ReadKey/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter();
+            ConsoleKeyInfo k;
+            do
+            {
+                k = Console.ReadKey();
+                c.Action(k);
+
+
+            } while (k.Key != ConsoleKey.Enter);
+        }
+    }
+}
